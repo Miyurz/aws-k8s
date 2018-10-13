0c31389909c9b788761ed4f7d832e702bc39b060
Mayur Nagekar <mayur@meetbeam.com>
2018-10-14 04:09:11 +0530

    Added a swithc statement to spin/nuke the k8s cluster at will

--- cluster.sh
+++ cluster.sh
@@ -75,49 +75,49 @@ function spin_k8s_cluster() {
   								  --kubernetes-version="1.11.2"  `# Version of kubernetes to run (defaults to version in channel)`\
 
  								  ##### GLOBAL OPTIONS #######
-  --alsologtostderr              `#log to standard error as well as files`\
-  #--config string               `#yaml config file (default is $HOME/.kops.yaml)`\
-  #--log_backtrace_at traceLocation   #when logging hits line file:N, emit a stack trace (default :0)
-  #--log_dir string                   #If non-empty, write log files in this directory
-  --logtostderr                  `#log to standard error instead of files (default false)`\
-  --stderrthreshold "severity"   `#logs at or above this threshold go to stderr (default 2)`\
-  --v=5 ;                        `#log level for V logs`\
-  #--vmodule moduleSpec          `#comma-separated list of pattern=N settings for file-filtered logging`\
+  								  --alsologtostderr              `#log to standard error as well as files`\
+  								  #--config string               `#yaml config file (default is $HOME/.kops.yaml)`\
+  								  #--log_backtrace_at traceLocation   #when logging hits line file:N, emit a stack trace (default :0)
+								  #--log_dir string                   #If non-empty, write log files in this directory
+								  --logtostderr                  `#log to standard error instead of files (default false)`\
+								  --stderrthreshold "severity"   `#logs at or above this threshold go to stderr (default 2)`\
+								  --v=5 ;                        `#log level for V logs`\
+								  #--vmodule moduleSpec          `#comma-separated list of pattern=N settings for file-filtered logging`\
 
-  #AWS resources
-  --admin-access=0.0.0.0/0       `# Restrict API access to this CIDR.  If not set, access will not be restricted by IP. (default [0.0.0.0/0])`\
-  --api-loadbalancer-type=public `#Sets the API loadbalancer type to either 'public' or 'internal' `\
-  #--api-ssl-certificate string  `# Currently only supported in AWS. Sets the ARN of the SSL Certificate to use for the API server loadbalancer.`\
-  --associate-public-ip          `# Specify --associate-public-ip=[true|false] to enable/disable association of public IP for master ASG and nodes. Default is 'true'.`\
-  --authorization RBAC           `# Authorization mode to use: AlwaysAllow or RBAC (default "RBAC")`\
-  --channel stable               `# Channel for default versions and configuration to use (default "stable"), More here: https://github.com/kubernetes/kops/tree/master/channels`\
-  --cloud-labels "test"  `# A list of KV pairs used to tag all instance groups in AWS (eg "Owner=John Doe,Team=Some Team").`\
-  --dns=private                  `# DNS hosted zone to use: public|private. Default is 'public'. (default "Public")`\
-  --dns-zone k8s."${ORG}".ai     `# DNS hosted zone to use (defaults to longest matching zone)`\
-  #--network-cidr string         `# Set to override the default network CIDR`\
-  --ssh-access=0.0.0.0/0         `# Restrict SSH access to this CIDR.  If not set, access will not be restricted by IP. (default [0.0.0.0/0])`\
-  #--subnets strings             `# Set to use shared subnets`\
-  --topology=public              `# Controls network topology for the cluster. public|private. Default is 'public'. (default "public")`\
-  #--utility-subnets strings         `# Set to use shared utility subnets`\
-  #Check if you want to use an exiting VPC?
-  #--vpc string                      `# Set to use a shared VPC`\
-  #-y, --yes                         `# Specify --yes to immediately create the cluster`\
-  
-  ############## MASTER NODE(S) ##########
-  --master-count 1                   `# Set the number of masters.  Defaults to one master per master-zone`\
-  --master-public-name string        `# Sets the public master public name`\
-  --master-security-groups strings   `# Add precreated additional security groups to masters.`\
-  --master-size m5.xlarge         `# Set instance size for masters`\
-  --master-tenancy dedicated      `# The tenancy of the master group on AWS. Can either be default or dedicated.`\
-  --master-volume-size 40         `# Set instance volume size (in GB) for masters`\
-  --master-zones 3                `# Zones in which to run masters (must be an odd number)`\
-  
-  ############# SLAVE NODE(S) ############
-  --node-count 3                   `# Set the number of nodes`\
-  #--node-security-groups strings `# Add precreated additional security groups to nodes.`\
-  --node-size m5d.4xlarge         `# Set instance size for nodes `\
-  --node-tenancy default          `# The tenancy of the node group on AWS. Can be either default or dedicated. `\
-  --node-volume-size 40           `# Set instance volume size (in GB) for nodes ` ;
+								  #AWS resources
+								  --admin-access=0.0.0.0/0       `# Restrict API access to this CIDR. Default being 0.0.0.0/0 `\
+								  --api-loadbalancer-type=public `#Sets the API loadbalancer type to either 'public' or 'internal' `\
+								  #--api-ssl-certificate string  `# Currently only supported in AWS.Sets the ARN of the SSL Certificate to use for the API server loadbalancer.`\
+								  --associate-public-ip          `# Specify --associate-public-ip=[true|false] to enable/disable association of public IP for master ASG & nodes.Default true`\
+								  --authorization RBAC           `#Authorization mode to use: AlwaysAllow or RBAC, default is "RBAC"`\
+								  --channel stable           `#Channel for default versions/configuration,default is stable.More here: https://github.com/kubernetes/kops/tree/master/channels`\
+								  --cloud-labels "test"      `# A list of KV pairs used to tag all instance groups in AWS (eg "Owner=John Doe,Team=Some Team").`\
+								  --dns=private                  `# DNS hosted zone to use: public|private. Default is 'public'. (default "Public")`\
+								  --dns-zone k8s."${ORG}".ai     `# DNS hosted zone to use (defaults to longest matching zone)`\
+								  #--network-cidr string         `# Set to override the default network CIDR`\
+								  --ssh-access=0.0.0.0/0         `# Restrict SSH access to this CIDR.  If not set, access will not be restricted by IP. (default [0.0.0.0/0])`\
+								  #--subnets strings             `# Set to use shared subnets`\
+								  --topology=public              `# Controls network topology for the cluster. public|private. Default is 'public'. (default "public")`\
+								  #--utility-subnets strings         `# Set to use shared utility subnets`\
+								  #Check if you want to use an exiting VPC?
+								  #--vpc string                      `# Set to use a shared VPC`\
+								  #-y, --yes                         `# Specify --yes to immediately create the cluster`\
+								  
+								  ############## MASTER NODE(S) ##########
+								  --master-count 1                   `# Set the number of masters.  Defaults to one master per master-zone`\
+								  --master-public-name string        `# Sets the public master public name`\
+								  --master-security-groups strings   `# Add precreated additional security groups to masters.`\
+								  --master-size m5.xlarge         `# Set instance size for masters`\
+								  --master-tenancy dedicated      `# The tenancy of the master group on AWS. Can either be default or dedicated.`\
+								  --master-volume-size 40         `# Set instance volume size (in GB) for masters`\
+								  --master-zones 3                `# Zones in which to run masters (must be an odd number)`\
+								  
+								  ############# SLAVE NODE(S) ############
+								  --node-count 3                   `# Set the number of nodes`\
+								  #--node-security-groups strings `# Add precreated additional security groups to nodes.`\
+								  --node-size m5d.4xlarge         `# Set instance size for nodes `\
+								  --node-tenancy default          `# The tenancy of the node group on AWS. Can be either default or dedicated. `\
+								  --node-volume-size 40           `# Set instance volume size (in GB) for nodes ` ;
 }
 
 function nuke_k8s_cluster() {
@@ -128,4 +128,28 @@ function nuke_k8s_cluster() {
 										  --state s3://s3-k8s-"${ORG}";
 }
 
-nuke_k8s_cluster
+function main() {
+  
+  if [ "$#" -gt "1" ]; then
+    echo ERROR: Please pass c for create or n for nuke
+    exit 1
+  fi
+
+  case $1 in
+	  c) 
+	    echo "INFO: Spinning the cluster"
+	    spin_k8s_cluster
+          ;;
+          n)
+	   echo "INFO: Nuking the cluster"
+	   nuke_k8s_cluster
+	  ;;
+          *)
+	   echo "ERROR: Uknown option. Run me as $0 c or $0 n"
+	   exit 1
+	  ;;
+  esac
+}
+
+
+main "$@"
